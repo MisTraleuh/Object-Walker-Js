982cc10d899d27966dca0f5ceba978ecac6b9f04
Nathan Catalan <nathan.catalan@epitech.eu>
2024-05-10 23:34:36 +0200

    [REMOVE] unless \n on --help

--- src/utils/displayHelp.js
+++ src/utils/displayHelp.js
@@ -1,14 +1,14 @@
 import { Colors } from "./colors.js";
 
 export function displayHelp() {
-    console.log(`
-${Colors.CYAN}Usage: node index.js [options]${Colors.RESET}
+    const helpMessage = `${Colors.CYAN}Usage: node index.js [options]${Colors.RESET}
 Options:
     --h, --help         Display help
     --type              Type of source object
     --target            Target object
     --targetType        Target type of object target
-    --depth             Depth of the search
-`);
+    --depth             Depth of the search (default: 5)`;
+
+    console.log(helpMessage);
     process.exit(0);
 }
